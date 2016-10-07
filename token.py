99a881984be9b7cbeb5c4477a43b74dcb54d10e9
lazyKindMan <longsqiang@gmail.com>
2016-10-07 15:55:02 +0800

    转到windows开发

--- token.py
+++ token.py
@@ -17,10 +17,35 @@ reserved={
 }
 
 #other token name
-token=(
-	'GT',
-	
-	)
+token=[
+	'GT',#bigger 
+	'LT',#littler
+	'EQ',#equal
+	'GTEQ',# be equal or greater than
+	'LTEQ',# be litter or greater than
+	'AND',#and
+	'OR',#or
+	'NOT'#not
 
+	]+list(reserved.values())
+
+#define the regluar
+def t_main(t):
+	r'main'
+	t.value=int(t.value)
+	return t
+	
+#build the lexer
+lexer=lex.lex()
 #recognize the string and get the token
-def token_recogn(lines):
+#def token_recogn(lines):
+
+lexer.input('main')
+
+if __name__=='__main__':
+	while True:
+		tok=lexer.token()
+		if not tok:break
+		print tok	
+
+http://www.pchou.info/open-source/2014/01/18/52da47204d4cb.html
